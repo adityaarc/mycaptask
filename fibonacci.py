69437d7e1826b5169bb12b187313117f67b4c69e
ADITYA RAJ CHOUDHARY <55029118+adityaarc@users.noreply.github.com>
2020-07-10 23:54:09 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+a = 0
+b = 1
+print(a)
+print(b)
+i=0
+
+while i<10:
+    i=i+1
+    c = a+b
+    print(c)
+    a = b
+    b = c
+
+
---
+++ positive.py
@@ -0,0 +1,5 @@
+list1 = [1 , 20 , -4 , 5 , 10 , -8 , 9 , 23]
+
+for number in list1:
+    if(number > 0):
+        print(number)
