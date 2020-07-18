d256453fd51377085025bf9609498110af109a1c
ADITYA RAJ CHOUDHARY <55029118+adityaarc@users.noreply.github.com>
2020-07-19 01:02:47 +0530

    Add files via upload

---
+++ ds.py
@@ -0,0 +1,37 @@
+mylist1 = [2, 5, 6, 3]
+mylist2 = [1, 6, 4, 8]
+
+print(" mylist1 X 3 :")
+print(mylist1*3)
+print("  ")
+
+print("mylist1+ mylist2 :")
+print(mylist1+mylist2)
+print("  ")
+
+tup = ('Aditya ', ' Harsh ', ' Anushka ', 'Ananya' )
+
+print(" LENGTH : ")
+print(len(tup))
+print("  ")
+
+print(" Index : ")
+print(tup[1])
+print("  ")
+
+
+dict = {"INC ": 55, "BJP": 300, "DMK": 20, "YSRCP": 22, "SP" :5 ,"BSP": 10  }
+
+print(" Dictionary Elements : ")
+print(dict)
+print("  ")
+
+print("dict[INC]")
+print(dict["INC "])
+print("  ")
+
+dict.pop("SP")
+print("dict.pop(SP)")
+print(dict)
+
+
